5bc5c6f1f85c19156d05e60762e082ff3150832d
Jonthesallz <gitboi56@outlook.com>
2022-04-12 22:57:16 +0100

    mmmm

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ app.use(express.static(path.join(__dirname,'public')));
 app.use(mongoSanitize());
 
 const sessionConfig = {
+    name:'session',
     secret: 'thisshouldbeabettersecrest',
     resave: false,
     saveUninitialized: true,
